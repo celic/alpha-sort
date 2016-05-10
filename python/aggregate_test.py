18e4a9935e02b4254bc027683f0518109be15ccc
Ubuntu-64 <westem@rpi.edu>
2016-05-09 19:06:35 -0700

    updated latex doc and added cludgey code to perform some testing.

--- python/aggregate_test.py
+++ python/aggregate_test.py
@@ -1,7 +1,7 @@
 import verify_list
 import argparse
 import time
-
+import numpy as np
 # Each test boosts the algorithm and records the correct rate
 def test(file_name, iterations, alpha, size, delta, error, timeIO):
 	average = []
@@ -14,10 +14,29 @@ def test(file_name, iterations, alpha, size, delta, error, timeIO):
 		io_time += io
 
 	avg = sum(average)/float(len(average))
-
 	if io_time > 0:
-		print "IO time: %.2fs" % (io_time)
-	return (float(results.count(True)) / float(iterations)), avg
+		print "IO time: %20.2fs" % (io_time)
+	# this 
+	return (float(results.count(True)) / float(iterations)), avg, (alpha-avg)/alpha
+
+def foo(file_name):
+	alpha = 0.92
+	iterations = 1
+	size = 1000000000
+	error = 0.1
+	delta = 0.25
+	avg_error = 0
+	count_bad = 0
+	max_error = 0
+	# for delta in np.arange(0.05,1,0.1):
+	for i in xrange(0, 1000):
+		s_ratio, avg, err = test(file_name, iterations, alpha, size, delta, error, False)
+		avg_error += abs(err)/1000.0
+		max_error = max(max_error, abs(err))
+		if abs(err) > error:
+			count_bad += 1
+
+	print "Average Error: %10.8f, Num out of bound: %d, max_error %10.8f" % (avg_error, count_bad, max_error)
 
 if __name__ == '__main__':
 	# Argument parsing
@@ -34,7 +53,7 @@ if __name__ == '__main__':
 	iterations = int(args.iterations)
 	alpha = float(args.alpha)
 	size = int(args.size)
-	delta = alpha = float(args.delta)
+	delta = float(args.delta)
 	error = float(args.error)
 	timeIO = bool(args.timeIO)
 
@@ -42,4 +61,4 @@ if __name__ == '__main__':
 	for i in xrange(1):
 		print test(file, iterations, alpha, size, delta, error, timeIO)
 	end = time.clock()
-	print "Total time: %.2fs" % (end - start)
+	print "Total time: %20.2fs" % (end - start)
